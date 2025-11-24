b99b558ef7e0cdf37017d0928027ad348038bc56
zeliecoupey <coupeyzelie@gmail.com>
2025-11-24 08:59:51 +0100

    test git

--- frontend/src/components/MapUploader.jsx
+++ frontend/src/components/MapUploader.jsx
@@ -15,7 +15,7 @@ export default function MapUploader({ onMapLoaded, onCancel }) {
     if (!file) return;
 
     if (!file.name.endsWith('.xml')) {
-      setError('Veuillez sélectionner un fichier XML valide.');
+      setError('Veuillez sélectionner un fichier XML valide svp.');
       return;
     }
 
